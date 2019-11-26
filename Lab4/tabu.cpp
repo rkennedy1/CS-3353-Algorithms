ea62b6acbb87e58441eb9e9f766a34f49b5fdfec
Ryan-Kennedy <kennedy@smu.edu>
2019-11-26 13:32:12 -0600

    add lab 4

---
+++ Lab4/tabu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ryan on 11/26/19.
+//
+
+#include "tabu.h"
---
+++ Lab4/tabu.h
@@ -0,0 +1,14 @@
+//
+// Created by Ryan on 11/26/19.
+//
+
+#ifndef LAB4_TABU_H
+#define LAB4_TABU_H
+
+
+class tabu {
+
+};
+
+
+#endif //LAB4_TABU_H
